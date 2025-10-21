3daf416871914f02645725ffb7ac96d1eb1a0139
JordanHolder-CS <jordangstars@hotmail.com>
2025-10-21 10:07:05 +0100

    Updated header styling

--- App.js
+++ App.js
@@ -1,22 +1,27 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import ModuleListScreen from "./src/components/screens/ModuleListScreen";
+
+const Stack = createNativeStackNavigator();
 
 export const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Modules"
+        screenOptions={{
+          headerStyle: { backgroundColor: "rgba(0, 0, 0, 1)" },
+          headerTintColor: "#ffffffff",
+        }}
+      >
+        <Stack.Screen
+          name="Modules"
+          component={ModuleListScreen}
+          options={{ title: "Module List" }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
 
 export default App;
