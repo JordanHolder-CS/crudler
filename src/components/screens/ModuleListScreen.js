f754db875e15db5db0dc5498914c40e2189fdde2
JordanHolder-CS <jordangstars@hotmail.com>
2025-10-23 22:47:20 +0100

    Items can now be deleted from the ModuleList

--- src/components/screens/ModuleListScreen.js
+++ src/components/screens/ModuleListScreen.js
@@ -1,17 +1,26 @@
 import { StatusBar } from "expo-status-bar";
 import { Pressable, ScrollView, StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
 import Screen from "../layout/screen.js";
 import initialModules from "../../data/modules.js";
 import ModuleList from "../Modules/ModuleList.js";
 
 export const ModuleListScreen = () => {
-  const modules = initialModules;
+  const [modules, setModules] = useState(initialModules);
 
   const handleSelect = () => alert("Course selected");
 
+  const handleDelete = (module) =>
+    setModules(modules.filter((item) => item.ModuleID !== module.ModuleID));
+
+  //modules =
+  console.log(
+    `After deleting ${module.ModuleCode}, modules length is ${modules.length}`
+  );
+
   return (
     <Screen>
-      <ModuleList modules={modules} onSelect={handleSelect} />
+      <ModuleList modules={modules} onSelect={handleDelete} />
     </Screen>
   );
 };
