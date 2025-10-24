49fbd1250cd72dc93aa958b8c4c175ad080e01e2
JordanHolder-CS <jordangstars@hotmail.com>
2025-10-24 02:42:54 +0100

    Moved view page logic to ModuleView.js

--- src/components/screens/ModuleViewScreen.js
+++ src/components/screens/ModuleViewScreen.js
@@ -3,28 +3,14 @@ import { StyleSheet, Text, View } from "react-native";
 import Screen from "../layout/screen";
 import FullWidthImage from "react-native-fullwidth-image";
 import ModuleListScreen from "./ModuleListScreen";
+import ModuleView from "../Modules/ModuleView.js";
 
 export const ModuleViewScreen = ({ navigate, route }) => {
   const { module } = route.params;
 
   return (
     <Screen>
-      <View style={styles.container}>
-        <FullWidthImage
-          source={{ uri: module?.ModuleImage }}
-          style={styles.image}
-        />
-        <View style={styles.infoTray}>
-          <Text style={styles.boldText}>
-            {module.ModuleCode} {module.ModuleName}
-          </Text>
-          <Text style={styles.text}>Level {module.ModuleLevel}</Text>
-          <Text style={styles.text}>
-            {module.ModuleLeaderName}{" "}
-            <Text style={styles.dimText}>(Module Leader)</Text>
-          </Text>
-        </View>
-      </View>
+      <ModuleView module={module} />
     </Screen>
   );
 };
